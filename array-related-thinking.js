029e7a57ac4c742f84f7ed3afc7777a56da5ae1d
Md-Sujon <mdsujon829562@gmail.com>
2021-12-04 21:21:34 +0600

    s

---
+++ array-related-thinking.js
@@ -0,0 +1,35 @@
+const name = ['Sujon', 'james', 'robin', 'jhon'];
+const age = [40, 50, 60];
+
+const products = [
+    {id:1, name: 'laptop', price: 200}, 
+    {id:2, name:'mobile', price:20}, 
+    {id:3, name:'tab', price:150},
+    {id:4, name:'Watch', price:120}
+];
+
+//Map
+// Single  hola return korta hoba na
+const names = products.map((product) =>product.name);
+
+// Multi hola return korta hoba
+const namess = products.map((product) =>{
+   return product.name
+});
+console.log(namess);
+
+// ForEach
+products.forEach((product) =>console.log(product.name))
+
+//filter
+const lowRate = products.filter((product) =>product.price > 100 );
+console.log(lowRate);
+
+// filter use remove item
+
+const removeItem = products.filter((product) =>product.id != 4);
+console.log(removeItem);
+
+
+
+
